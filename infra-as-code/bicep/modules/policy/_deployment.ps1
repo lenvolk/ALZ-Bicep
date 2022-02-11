a90d61eb3691dd10ab733698a4c2311c623bd0ef
Len Volk <levolkov@microsoft.com>
2022-02-11 15:22:03 -0500

    update run ps1 lv

--- infra-as-code/bicep/modules/policy/_deployment.ps1
+++ infra-as-code/bicep/modules/policy/_deployment.ps1
@@ -9,4 +9,4 @@ New-AzManagementGroupDeployment `
   -TemplateFile .\definitions\custom-policy-definitions.bicep `
   -TemplateParameterFile .\definitions\custom-policy-definitions.parameters.example.json `
   -Location eastus2 `
-  -ManagementGroupId 'a659fe98-5688-4592-99cd-0be85cd550bc'
+  -ManagementGroupId 'volk'
