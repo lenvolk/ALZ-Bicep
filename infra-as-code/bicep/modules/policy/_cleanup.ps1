5bc7e5f4a998e7993377a8706e15e740a7bf39ec
Len Volk <levolkov@microsoft.com>
2022-02-15 09:28:47 -0500

    cleanup

--- infra-as-code/bicep/modules/policy/_cleanup.ps1
+++ infra-as-code/bicep/modules/policy/_cleanup.ps1
@@ -2,31 +2,35 @@
 # 
 # list all definitions by MgGrp ==> Get-AzPolicySetDefinition -ManagementGroupName $ManagementGrpName
 
-$InitiativeName = 'Enforce-Encryption-CMK' #'Configure Azure PaaS services to use private DNS zones'
+$InitiativeNames = 'Deny-PublicPaaSEndpoints','Deploy-ASCDF-Config','Deploy-Diagnostics-LogAnalytics','Deploy-Private-DNS-Zones','Deploy-Sql-Security','Enforce-Encryption-CMK','Enforce-EncryptTransit'
 $ManagementGrpName = 'volk'
 
-param(
-    [Parameter(Mandatory = $true)][string]$InitiativeName,
-    [Parameter(Mandatory = $true)][string]$ManagementGrpName
-)
+# param(
+#     [Parameter(Mandatory = $true)][string]$InitiativeNames,
+#     [Parameter(Mandatory = $true)][string]$ManagementGrpName
+# )
  
-$initiative = Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName #-Custom -ErrorAction Ignore
+foreach ($InitiativeNames in $InitiativeName) {
+
+$initiative = Get-AzPolicySetDefinition -Name $InitiativeNames -ManagementGroupName $ManagementGrpName -ErrorAction Ignore #-Custom
 if ($null -ne $initiative) {
 
-            Write-Output "Assignment for initiative $InitiativeName will be removed as RemoveAssignment parameter was set to true."
+            Write-Output "Assignment for initiative $InitiativeNames will be removed as RemoveAssignment parameter was set to true."
             Remove-AzPolicyAssignment -Id $assignment.PolicyAssignmentId | Out-Null
             Write-Output "Assignment $($assignment.Name) has been removed."           
  
-            $Policies = (Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName).Properties.policyDefinitions.policyDefinitionId
-            Remove-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName -Force | Out-Null
-            Write-Output "Initiative $InitiativeName has been removed."
+            $Policies = (Get-AzPolicySetDefinition -Name $InitiativeNames -ManagementGroupName $ManagementGrpName).Properties.policyDefinitions.policyDefinitionId
+            Remove-AzPolicySetDefinition -Name $InitiativeNames -ManagementGroupName $ManagementGrpName -Force | Out-Null
+            Write-Output "Initiative $InitiativeNames has been removed."
  
             foreach ($policy in $Policies) {
-                Write-Output "Removing policy $policy assgined to $InitiativeName"
-                Remove-AzPolicyDefinition -Id $policy -Force | Out-Null
+                Write-Output "Removing policy $policy assgined to $InitiativeNames"
+                Remove-AzPolicyDefinition -Id $policy -Force -ErrorAction Ignore #| Out-Null
                 Write-Output "Policy $policy has been removed."
             }  
         }
 else {
-    Write-Output "Initiative $InitiativeName not found."
+    Write-Output "Initiative $InitiativeNames not found."
+}
+
 }
