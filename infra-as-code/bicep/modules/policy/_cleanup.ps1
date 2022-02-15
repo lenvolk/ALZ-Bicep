eb6edd139be588a4f61442d718256f325753c7cb
Len Volk <levolkov@microsoft.com>
2022-02-15 12:24:36 -0500

    Chris Martin helped to imporove the script!!!

--- infra-as-code/bicep/modules/policy/_cleanup.ps1
+++ infra-as-code/bicep/modules/policy/_cleanup.ps1
@@ -2,7 +2,7 @@
 # 
 # list all definitions by MgGrp ==> Get-AzPolicySetDefinition -ManagementGroupName $ManagementGrpName
 
-$InitiativeNames = 'Deny-PublicPaaSEndpoints','Deploy-ASCDF-Config','Deploy-Diagnostics-LogAnalytics','Deploy-Private-DNS-Zones','Deploy-Sql-Security','Enforce-Encryption-CMK','Enforce-EncryptTransit'
+$InitiativeNames = 'Deploy-Diagnostics-LogAnalytics','Deploy-ASCDF-Config','Deny-PublicPaaSEndpoints','Deploy-Private-DNS-Zones','Deploy-Sql-Security','Enforce-Encryption-CMK','Enforce-EncryptTransit'
 $ManagementGrpName = 'volk'
 
 # param(
@@ -16,15 +16,15 @@ $initiative = Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupNa
 if ($null -ne $initiative) {
 
             Write-Output "Assignment for initiative $InitiativeName will be removed as RemoveAssignment parameter was set to true."
-            Write-Output "Assignment $($assignment.Name) has been removed."           
  
-            $Policies = (Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName).Properties.policyDefinitions.policyDefinitionId
+            $Policies = (Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName).Properties | where -property policytype -eq custom
+            $Policies = $Policies.policyDefinitions.policyDefinitionId
             Remove-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName -Force | Out-Null
             Write-Output "Initiative $InitiativeName has been removed."
  
             foreach ($policy in $Policies) {
                 Write-Output "Removing policy $policy assgined to $InitiativeName"
-                Remove-AzPolicyDefinition -Id $policy -Force -ErrorAction Ignore #| Out-Null
+                Remove-AzPolicyDefinition -Id $policy -Force -ErrorAction Ignore
                 Write-Output "Policy $policy has been removed."
             }  
         }
