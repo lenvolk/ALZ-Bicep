7789b5dc55b72c22f55cbae8f424552d8d0dd126
Len Volk <levolkov@microsoft.com>
2022-02-14 20:48:26 -0500

    added policy clean up script

---
+++ infra-as-code/bicep/modules/policy/_cleanup.ps1
@@ -0,0 +1,33 @@
+
+# REF: https://www.powershellbros.com/remove-azure-initiative-with-related-policies/
+
+
+$InitiativeName = 'Enforce-Encryption-CMK' #'Configure Azure PaaS services to use private DNS zones'
+$ManagementGrpName = 'volk'
+#$RemoveAssignment = 'false'
+
+param(
+    [Parameter(Mandatory = $true)][string]$InitiativeName,
+    [Parameter(Mandatory = $true)][string]$ManagementGrpName
+)
+ 
+$initiative = Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName #-Custom -ErrorAction Ignore
+if ($null -ne $initiative) {
+
+            Write-Output "Assignment for initiative $InitiativeName will be removed as RemoveAssignment parameter was set to true."
+            Remove-AzPolicyAssignment -Id $assignment.PolicyAssignmentId | Out-Null
+            Write-Output "Assignment $($assignment.Name) has been removed."           
+ 
+            $Policies = (Get-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName).Properties.policyDefinitions.policyDefinitionId
+            Remove-AzPolicySetDefinition -Name $InitiativeName -ManagementGroupName $ManagementGrpName -Force | Out-Null
+            Write-Output "Initiative $InitiativeName has been removed."
+ 
+            foreach ($policy in $Policies) {
+                Write-Output "Removing policy $policy assgined to $InitiativeName"
+                Remove-AzPolicyDefinition -Id $policy -Force | Out-Null
+                Write-Output "Policy $policy has been removed."
+            }  
+}
+else {
+    Write-Output "Initiative $InitiativeName not found."
+}
